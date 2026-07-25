70173a92fb811f9e98bd25103f6cc5616b9e84ed
SailorJupiterUwU <anaelapozo192@gmail.com>
2026-07-25 10:07:18 -0500

    Update api importation ad=t usePresupuesto hook

--- fronted/src/hooks/usePresupuesto.js
+++ fronted/src/hooks/usePresupuesto.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from 'react';
-import api from '../utils/apit';
+import api from '../utils/api';
 import { ENDPOINTS } from '../utils/endpoints';
 import { handleApiError } from '../utils/helpers';
 
