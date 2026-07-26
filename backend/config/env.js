528db892d344669c2ab67d2b151eb9618f7b3d8f
SailorJupiterUwU <anaelapozo192@gmail.com>
2026-07-26 18:22:15 -0500

    Add dockerfile and update enviromental vairbales

--- backend/config/env.js
+++ backend/config/env.js
@@ -7,6 +7,7 @@ module.exports = {
         pass: process.env.DB_PASS || "root",
         host: process.env.DB_HOST || "localhost",
         port: process.env.DB_PORT || 3306,
-        dialect: process.env.DB_DIALECT || "mysql"
+        dialect: process.env.DB_DIALECT || "mysql",
+        sslCa: process.env.DB_SSL_CA || null
     }
 };
